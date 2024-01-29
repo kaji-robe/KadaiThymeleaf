5dc46fa23d60968adb60d37b317d169795626ca0
kaji-robe <hayano3.141592653@gmail.com>
2024-01-30 02:12:55 +0900

    2024/01/30 2:12

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class InputController {
-    @GetMapping("/input{valOut}")
+    @GetMapping("/input?previous={valOut}")
     public String getInput(@PathVariable String valOut, Model model) {
         // パスパラメータで受け取った値をModelに登録
         model.addAttribute("valOut", valOut);
