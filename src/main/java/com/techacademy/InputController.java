6ee667601d824d370ea7d4131c679fc481c1d035
kaji-robe <hayano3.141592653@gmail.com>
2024-01-29 11:54:32 +0900

    2024/01/29 11:54

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -17,3 +17,4 @@ public class InputController {
         return "input";
         }
 }
+
--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -5,6 +5,28 @@
 
 package com.techacademy;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class OutputController {
+
+
+    @GetMapping("/input")
+    public String getInput() {
+        //input.htmlに画面遷移
+        return "input";
+    }
+
+
+    @PostMapping("/confirm")
+    public String postOutput(@RequestParam("val") String val, Model model) {
+        //フォームから送信されてきた値をModelに登録
+        model.addAttribute("val", val);
+        //output.htmlに画面遷移
+        return "confirm";
+    }
+
 
 }
