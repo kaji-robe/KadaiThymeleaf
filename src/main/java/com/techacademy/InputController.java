2c0e64f628aecc44fed282bda12fb64f13742267
kaji-robe <hayano3.141592653@gmail.com>
2024-01-30 14:45:35 +0900

    2024/01/30 14:45

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -16,19 +16,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class InputController {
 
-        @GetMapping("/input{valOut}")
-         public String getInput(@PathVariable String valOut, Model model) {
-        // パスパラメータで受け取った値をModelに登録
-         model.addAttribute("valOut", valOut);
-        // input.htmlに画面遷移
-              return "input";
-        }
+    @GetMapping("/input{valOut}")
+     public String getInput(@PathVariable String valOut, Model model) {
+    // パスパラメータで受け取った値をModelに登録
+     model.addAttribute("valOut", valOut);
+    // input.htmlに画面遷移
+          return "input";
+    }
 
-    //            @GetMapping("/input")
-    //            public String getInput() {
-    //            //input.htmlに画面遷移
-    //                return "input";
-    //                }
+//            @GetMapping("/input")
+//            public String getInput() {
+//            //input.htmlに画面遷移
+//                return "input";
+//                }
 }
 
 
